61e768632fff9b5887b1c0f72b624ddd13c102f8
DeepChak <127760974+DeepChak@users.noreply.github.com>
2023-04-08 19:13:45 +0530

    Add files via upload

---
+++ Arrowfunction.js
@@ -0,0 +1,78 @@
+// // Print odd numbers in an array using Arrow funtion.
+// const numbers = [8, 19, 5, 6, 14, 9, 13];
+// const odds = [];
+// numbers.forEach((num) => {
+//   if (num % 2 === 1) {
+//     odds.push(num);
+//   }
+// });console.log(odds);
+
+// // 2.Convert all the strings to title caps in a string array using arrow function.
+
+// let stringArray = ['krishna', 'udesh', 'deepan', 'karthi', 'prabhu'];
+
+// let StringArray = (function (stringArray) {
+
+//     return (stringArray.map((item) => {
+
+//         return item.toUpperCase();
+
+//     }))
+// })(stringArray);
+
+// console.log(StringArray);
+
+//1. Print odd numbers in an array
+
+// let numberArray = [1, 2, 3, 4, 5, 6];
+
+// console.log("Odd Numbers : " + numberArray.filter((item) => {
+
+//     return item % 2 !== 0
+
+// }))
+
+// //2.Convert all the strings to title caps in a string array
+
+// let stringArray = ["deepan", "is", "a", "smart", "boy"];
+
+// stringArray = stringArray.map((item) => {
+
+//     return item.toUpperCase()
+// })
+// console.log(stringArray);
+
+// //3.Sum of all numbers in an array
+
+// let numberArray1 = [1, 2, 3, 4, 5, 6];
+// let sum = numberArray1.reduce((currentTotal, item) => {
+//     return currentTotal + item
+// }, 0)
+// console.log(sum)
+
+// //4.Return all the prime numbers in an array
+
+// let numberarr = [1, 2, 3, 4, 5, 6];
+// console.log("Prime Number" +
+//     numberarr.filter((item) => {
+//         for (let i = 2; i < item; i++) {
+//             if (item % i === 0)
+//                 return false;
+//         }
+//         return item !== 1;
+
+//     }))
+
+// //5.Return all the palindromes in an array 
+
+// let arrayPallindrome = [1, 2, 3, 4, 5, 6, 11, 12, 13,14,15,16,17,18,19,20,21,22];
+
+// console.log("Pallindrome Numbers" +
+//     arrayPallindrome.filter((item) => {
+//         let temp = item + "";
+//         if (temp.split('').reverse().join('') === item + "")
+//             return true
+
+//         return false
+//     }))
+
